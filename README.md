20c1f0a3729557d0e47843ff889e925812f7b1da
vince-fugnitto <vincent.fugnitto@ericsson.com>
2021-02-19 14:36:16 -0500

    Initial Commit
    Signed-off-by: vince-fugnitto <vincent.fugnitto@ericsson.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+# VS Code Multi-Root Test
+
+## Description
+
+The following extension tests the `vscode.workspace.workspaceFile` API, through the `Echo Workspace` command.
---
+++ src/extension.ts
@@ -0,0 +1,17 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand(
+		'vscode-workspace-file.echoWorkspace', () => {
+			const workspace = vscode.workspace.workspaceFile;
+			if (workspace) {
+				vscode.window.showInformationMessage(`Workspace File: ${workspace}`);
+			}
+			vscode.window.showErrorMessage(`No Workspace File Present.`);
+		}
+	);
+
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() { }
